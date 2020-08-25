88a6afd2700dc6c692f99f6747d33e235eb640a5
Rohan Bojja <rohanbojja@outlook.com>
2020-08-25 15:55:12 +0530

    - Fix appengine config - Pass a String with length < 6 as IdToken header to skip Auth (Testing)

--- src/main/java/org/legalaidcamp/server/services/AuthenticationServiceImpl.java
+++ src/main/java/org/legalaidcamp/server/services/AuthenticationServiceImpl.java
@@ -4,6 +4,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseToken;
 import org.legalaidcamp.server.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,15 +12,25 @@ import java.util.List;
 @Service
 public class AuthenticationServiceImpl implements AuthenticationService{
 
+    @Autowired
+    UtilityService utilityService;
+
     @Override
     public String getUid(String idToken) throws IllegalArgumentException,FirebaseAuthException{
-        FirebaseToken decodedToken;
-        try {
-            decodedToken = FirebaseAuth.getInstance().verifyIdToken(idToken);
-            return decodedToken.getUid();
-        } catch (FirebaseAuthException | IllegalArgumentException e) {
-            e.printStackTrace();
-            throw e;
+        /*
+        TODO: Remove "dev" in production!
+         */
+        if(idToken.length()<6){
+            return idToken;
+        }else{
+            FirebaseToken decodedToken;
+            try {
+                decodedToken = FirebaseAuth.getInstance().verifyIdToken(idToken);
+                return decodedToken.getUid();
+            } catch (FirebaseAuthException | IllegalArgumentException e) {
+                e.printStackTrace();
+                throw e;
+            }
         }
     }
 
